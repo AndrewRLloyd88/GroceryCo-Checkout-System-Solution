6445413af479415d416ca2a7a4f64ef30b5e9faf
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-12-14 17:28:48 -0800

    write out basic logic using readline to write in a list of items

--- checkout.js
+++ checkout.js
@@ -1,3 +1,54 @@
-const items = process.argv.slice(2);
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-console.log(items);
+//holds the shopping cart
+const shoppingCart = [];
+//declare a boolean to see if we are done entering items
+let scanning = true;
+
+//ask initial question to user
+const askQuestion = () => {
+  rl.question(
+    'Welcome to GroceryCo please scan in your items, if you are done scanning type done. ',
+    (item) => {
+      console.log(item);
+      if (item === 'done') {
+        rl.close();
+      } else {
+        shoppingCart.push(item);
+        askNextQuestion();
+      }
+    }
+  );
+};
+
+//asks this question the next time around
+const askNextQuestion = () => {
+  rl.question(
+    'Please scan in your next item, if you are done scanning type done. ',
+    (item) => {
+      console.log(item);
+      if (item === 'done') {
+        rl.close();
+      } else {
+        shoppingCart.push(item);
+        askQuestion();
+      }
+    }
+  );
+};
+
+if (scanning) {
+  askQuestion();
+} else {
+  rl.close();
+}
+
+//return the itemized grocery list with totals here
+rl.on('close', () => {
+  console.log(shoppingCart);
+  process.exit(0);
+});
