bd981fa6b7f1b87fec78ef3cd64b8326720b8219
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-12-16 12:16:45 -0800

    add chalk to dependencies and test conditional color formatting

--- helpers/formatUnitPrices.js
+++ helpers/formatUnitPrices.js
@@ -1,6 +1,12 @@
+const chalk = require('chalk');
+
 const formatUnitPrices = (obj) => {
+  // console.log(obj)
 for(price in obj){
-  console.log(`${price}: $${obj[price].toFixed(2)}`)
+  const currentItem = obj[price]
+  const currentItemPrice = currentItem.currentPrice
+  const previousItemPrice = currentItem.previousPrice
+  console.log(`${price}: ${previousItemPrice ? (chalk.green('now: $' + currentItemPrice.toFixed(2))) : currentItemPrice.toFixed(2)} ${previousItemPrice ? (chalk.red('was: $' + previousItemPrice.toFixed(2))) : ''}`)
 }
 }
 
